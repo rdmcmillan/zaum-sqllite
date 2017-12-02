2b094296cf4844e60bf4decc5250fbca8a9543b2
Dave Doherty <dadoherty@gmail.com>
2017-12-02 12:23:46 -0500

    initial commit of the sqlite adapter

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject zaum-sqlite "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure    "1.9.0-RC1"]
+                 [org.zaumdb/zaum        "0.1.0-SNAPSHOT"]
+                 [org.clojure/java.jdbc  "0.7.3"]
+                 [org.xerial/sqlite-jdbc "3.21.0"]])
