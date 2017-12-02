441619bd47771cb3f44ecaa4aee42168ec9a9120
Damon Kropf-Untucht <dkropfuntucht@teksavvy.com>
2017-12-02 13:59:43 -0500

    Add early adapter including unwanted test.db

--- src/zaum_sqlite/core.clj
+++ src/zaum_sqlite/core.clj
@@ -1,6 +1,36 @@
-(ns zaum-sqlite.core)
+(ns zaum-sqlite.core
+  (:require [clojure.java.jdbc :as jdbc]
+            [zaum.core :as z]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def search-types
+  { > >
+   :> >})
+
+(defn- construct-filter
+  [identifier]
+  (fn [v]
+    (every?
+     (fn [filter-spec]
+       (let [[filter-key filter-search] filter-spec]
+         (and (contains? v filter-key)
+              (cond (vector? filter-search)
+                    ((search-types (first filter-search))
+                     (filter-key v)
+                     (second filter-search))
+                    :or
+                    (= (filter-key v) filter-search)))))
+     identifier)))
+
+(defrecord ZaumSQLite [connection]
+  z/IZaumDatabase
+  (perform-get [_ {:keys [entity identifier]}]
+    (cond
+      (nil? identifier)
+      (jdbc/query @connection [(str "SELECT * from " entity)])
+      (map? identifier)
+      (vec (filter (construct-filter identifier) (@connection entity))))))
+
+;;TODO: this is a stop-gap - eventually we need to use the connection map
+(defn new-sqlite
+  [connection-map]
+  (ZaumSQLite. (atom connection-map)))
--- test/zaum_sqlite/core_test.clj
+++ test/zaum_sqlite/core_test.clj
@@ -1,7 +1,111 @@
 (ns zaum-sqlite.core-test
   (:require [clojure.test :refer :all]
+            [zaum.core :as z]
             [zaum-sqlite.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def test-data
+  {:table-0
+   [{:created-at 0 :updated-at 1 :text "foo" :other-val 4}
+    {:created-at 1 :updated-at 1 :text "foo" :other-val 7}
+    {:created-at 2 :updated-at 2 :text "bar" :other-val 5}
+    {:created-at 3 :updated-at 3 :text "baz" :other-val 6}]})
+
+(deftest test-get-all
+  (testing "Basic test of getting all records in a table"
+    (let [result (z/perform-op :get
+                               {:operation  :get
+                                ;;TODO: how do our connection strings work?
+                                :connection {:dbtype "sqlite"
+                                             :dbname "test.db"
+                                             :impl
+                                             (new-sqlite {:dbtype "sqlite"
+                                                          :dbname "test.db"})}
+                                :entity     "test"})]
+      (is (= :ok (:status result)))
+      (is (= (:count result) (count (:data result)))))))
+
+(deftest test-get-by-identifier
+  (testing "Test getting one entry by an identifier one key"
+    (let [result (z/perform-op :get
+                               {:operation  :get
+                                :connection {:impl (new-sqlite test-data)}
+                                :entity     :table-0
+                                :identifier {:created-at 0}})]
+      (is (= :ok (:status result)))
+      (is (= (:count result)) (count (:data result)))
+      (is (= (get-in result [:data 0 :other-val]) 4))
+      (is (= (:count result) 1))))
+  (testing "Test getting one entry by an identifier w/ multiple keys"
+    (let [result (z/perform-op :get
+                               {:operation  :get
+                                :connection {:impl (new-sqlite test-data)}
+                                :entity     :table-0
+                                :identifier {:updated-at 1
+                                             :created-at 1}})]
+      (is (= :ok (:status result)))
+      (is (= (:count result)) (count (:data result)))
+      (is (= (get-in result [:data 0 :other-val]) 7))
+      (is (= (:count result) 1))))
+  (testing "Test getting multiple entries by an identifier w/ multiple keys"
+    (let [result (z/perform-op :get
+                               {:operation  :get
+                                :connection {:impl (new-sqlite test-data)}
+                                :entity     :table-0
+                                :identifier {:text       "foo"
+                                             :updated-at 1}})]
+      (is (= :ok (:status result)))
+      (is (= (:count result)) (count (:data result)))
+      (is (= (:count result) 2))))
+  (testing "Test getting no results for non-matching identifier clause"
+    (let [result (z/perform-op :get
+                               {:operation  :get
+                                :connection {:impl (new-sqlite test-data)}
+                                :entity     :table-0
+                                :identifier {:updated-at 10}})]
+      (is (= :ok (:status result)))
+      (is (= (:count result)) (count (:data result)))
+      (is (zero? (:count result))))))
+
+(deftest test-greater-than
+  (testing "test basic > performance in memory"
+    (let [result (z/perform-op :get
+                               {:operation  :get
+                                :connection {:impl (new-sqlite test-data)}
+                                :entity     :table-0
+                                :identifier {:updated-at [> 1]}})]
+      (is (= :ok (:status result)))
+      (is (= (:count result) 2))
+      (is (= (:count result) (count (:data result))))
+      (is (= "bar" (-> result :data first :text)))
+      (is (= "baz" (-> result :data second :text)))))
+  (testing "test basic :> performance in memory"
+    (let [result (z/perform-op :get
+                               {:operation  :get
+                                :connection {:impl (new-sqlite test-data)}
+                                :entity     :table-0
+                                :identifier {:updated-at [:> 1]}})]
+      (is (= :ok (:status result)))
+      (is (= (:count result) 2))
+      (is (= (:count result) (count (:data result))))
+      (is (= "bar" (-> result :data first :text)))
+      (is (= "baz" (-> result :data second :text)))))
+  (testing "test :> performance in memory returning nothing"
+    (let [result (z/perform-op :get
+                               {:operation  :get
+                                :connection {:impl (new-sqlite test-data)}
+                                :entity     :table-0
+                                :identifier {:updated-at [:> 3]}})]
+      (is (= :ok (:status result)))
+      (is (zero? (:count result)))
+      (is (= (:count result) (count (:data result))))))
+  (testing "test :> performance in memory returning across two keys"
+    (let [result (z/perform-op :get
+                               {:operation  :get
+                                :connection {:impl (new-sqlite test-data)}
+                                :entity     :table-0
+                                :identifier {:updated-at [:> 1]
+                                             :other-val  [:> 5]}})]
+      (is (= :ok (:status result)))
+      (is (= (:count result) 1))
+      (is (= (:count result) (count (:data result))))
+      (is (= "baz" (-> result :data first :text))))))
